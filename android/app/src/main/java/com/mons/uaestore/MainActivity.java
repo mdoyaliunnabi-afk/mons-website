abacbb25a1782a5de50e9c82751d2b764c58d157
mdoyaliunnabi-afk <mdoyaliun.nabi@gmail.com>
2026-09-11 00:51:40 +0400

    Add MONS Android WebView app

---
+++ android/app/src/main/java/com/mons/uaestore/MainActivity.java
@@ -0,0 +1,45 @@
+package com.mons.uaestore;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccess(false);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                Uri uri = request.getUrl();
+                String scheme = uri.getScheme();
+                if (scheme != null && !scheme.equals("http") && !scheme.equals("https")) {
+                    try { startActivity(new Intent(Intent.ACTION_VIEW, uri)); } catch (Exception ignored) {}
+                    return true;
+                }
+                return false;
+            }
+        });
+        webView.loadUrl("https://mdoyaliunnabi-afk.github.io/mons-website/");
+
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override public void handleOnBackPressed() {
+                if (webView.canGoBack()) webView.goBack(); else finish();
+            }
+        });
+    }
+}
