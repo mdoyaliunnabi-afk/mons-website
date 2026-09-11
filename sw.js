8de7be750a3c7f7ce391a889a89cfce332fd14e6
mdoyaliunnabi-afk <mdoyaliun.nabi@gmail.com>
2026-09-11 14:46:38 +0400

    Refresh MONS PWA cache and offline pages

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE='mons-v1';
-const ASSETS=['./','./index.html','./manifest.json','./mons-logo.svg','./product-gallery.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const copy=x.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return x}).catch(()=>caches.match('./')))));
+const CACHE='mons-v2';const ASSETS=['./','./index.html','./cart.html','./view.html','./about.html','./contact.html','./login.html','./signup.html','./manifest.webmanifest','./mons-logo.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(x=>x.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))))});
