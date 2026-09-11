862a93583d6df1cbf08eda03b2c597ebfce4e3d4
mdoyaliunnabi-afk <mdoyaliun.nabi@gmail.com>
2026-09-11 14:49:13 +0400

    Set MONS app ID to match mons.com branding

--- mobile-app/capacitor.config.ts
+++ mobile-app/capacitor.config.ts
@@ -1,3 +1,3 @@
 import type { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig={appId:'com.mons.uae',appName:'MONS',webDir:'../'};
+const config: CapacitorConfig={appId:'com.mons',appName:'MONS',webDir:'../'};
 export default config;
